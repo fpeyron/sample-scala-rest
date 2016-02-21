177e0625292d96cf13cc89c444300f49a340fca5
Florent Peyron <florentpeyron@gmail.com>
2016-02-21 17:59:22 +0100

    remove default config

--- src/main/scala/fr/sysf/sample/SampleRestApplication.scala
+++ src/main/scala/fr/sysf/sample/SampleRestApplication.scala
@@ -1,7 +1,14 @@
 package fr.sysf.sample
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.{ComponentScan, Configuration}
 
 object SampleRestApplication extends App {
   SpringApplication.run(classOf[BootConfig]);
 }
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+class BootConfig 
